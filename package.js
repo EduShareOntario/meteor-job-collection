e9b93388bc03bfc17e4906c24d40e34b74e5ed74
Lee Pender <lpender@gmail.com>
2018-11-30 12:51:00 -0500

    Version

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 ###     See included LICENSE file for details.
 ***************************************************************************/
 
-var currentVersion = '1.5.2';
+var currentVersion = '1.5.3';
 
 Package.describe({
   summary:
