5a3b754f8a60447ac6380100a31e924b3764fef1
Lee Pender <lpender@gmail.com>
2018-11-30 12:48:41 -0500

    Take ownership

--- package.js
+++ package.js
@@ -7,32 +7,33 @@
 var currentVersion = '1.5.2';
 
 Package.describe({
-  summary: "A persistent and reactive job queue for Meteor, supporting distributed workers that can run anywhere",
-  name: 'vsivsi:job-collection',
+  summary:
+    'A persistent and reactive job queue for Meteor, supporting distributed workers that can run anywhere',
+  name: 'lpender:job-collection',
   version: currentVersion,
-  git: 'https://github.com/vsivsi/meteor-job-collection.git'
+  git: 'https://github.com/vsivsi/meteor-job-collection.git',
 });
 
 Package.onUse(function(api) {
-  api.use('mrt:later@1.6.1', ['server','client']);
-  api.use('coffeescript@2.0.3_3', ['server','client']);
-  api.use('mongo@1.1.18', ['server','client']);
-  api.use('check@1.2.5', ['server','client']);
-  api.addFiles('job/src/job_class.coffee', ['server','client']);
-  api.addFiles('src/shared.coffee', ['server','client']);
+  api.use('mrt:later@1.6.1', ['server', 'client']);
+  api.use('coffeescript@2.0.3_3', ['server', 'client']);
+  api.use('mongo@1.1.18', ['server', 'client']);
+  api.use('check@1.2.5', ['server', 'client']);
+  api.addFiles('job/src/job_class.coffee', ['server', 'client']);
+  api.addFiles('src/shared.coffee', ['server', 'client']);
   api.addFiles('src/server.coffee', 'server');
   api.addFiles('src/client.coffee', 'client');
   api.export('Job');
   api.export('JobCollection');
 });
 
-Package.onTest(function (api) {
-  api.use('vsivsi:job-collection@' + currentVersion, ['server','client']);
-  api.use('mrt:later@1.6.1', ['server','client']);
-  api.use('coffeescript@2.0.3_3', ['server','client']);
-  api.use('check@1.2.5', ['server','client']);
-  api.use('tinytest@1.0.12', ['server','client']);
-  api.use('test-helpers@1.0.11', ['server','client']);
+Package.onTest(function(api) {
+  api.use('vsivsi:job-collection@' + currentVersion, ['server', 'client']);
+  api.use('mrt:later@1.6.1', ['server', 'client']);
+  api.use('coffeescript@2.0.3_3', ['server', 'client']);
+  api.use('check@1.2.5', ['server', 'client']);
+  api.use('tinytest@1.0.12', ['server', 'client']);
+  api.use('test-helpers@1.0.11', ['server', 'client']);
   api.use('ddp@1.2.5', 'client');
   api.addFiles('test/job_collection_tests.coffee', ['server', 'client']);
 });
